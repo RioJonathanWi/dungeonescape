ae2bee43b0bc81f932a30c1ee9b62088aca66e20
RioJonathanWi <105476962+RioJonathanWi@users.noreply.github.com>
2023-03-31 22:59:57 +0700

    Add files via upload

---
+++ assets/map/dungeonMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="dungeonMap" tilewidth="32" tileheight="32" tilecount="512" columns="32">
+ <image source="dungeonMap.png" width="1024" height="512"/>
+</tileset>
---
+++ assets/map/tilesetbaru.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="tilesetbaru" tilewidth="32" tileheight="32" tilecount="96" columns="24">
+ <image source="pacman-tileset.png" width="768" height="128"/>
+</tileset>
