3ca723cb755171f37aff1a733ad95e683d56d21b
RioJonathanWi <105476962+RioJonathanWi@users.noreply.github.com>
2023-03-31 23:20:11 +0700

    Add files via upload

---
+++ desktop/build/resources/main/map/dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="dungeon" tilewidth="32" tileheight="32" tilecount="504" columns="14">
+ <image source="dungeon.png" width="448" height="1152"/>
+</tileset>
